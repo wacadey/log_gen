fa7fefd72a132b2673d8b373c97d62ddcf7b4622
wacadey <wacadey@gmail.com>
2026-08-21 14:48:10 +0900

    feat: Flink cloudwatch 로그 관련 연결

--- infra/logs.tf
+++ infra/logs.tf
@@ -1,5 +1,12 @@
-# fargate를 통해 task 작동 => stdout/stderr등 발생 -> Cloudwatch에 저장
-resource "aws_cloudwatch_log_group" "generator" {
-  name              = local.log_group_name
+# Flink => CloudWatch Logs 기록
+resource "aws_cloudwatch_log_group" "flink" {
+  name              = local.flink_log_group_name
   retention_in_days = var.log_retention_days # 보관 기간 이후 로그 자동 삭제
+}
+
+# 실시간 처리 =>  log stream  생성
+resource "aws_cloudwatch_log_stream" "flink" {
+  name              = local.flink_log_stream_name
+  # 해당 스트림에 속할 그룹 지정
+  log_group_name    = aws_cloudwatch_log_group.flink.name
 }
