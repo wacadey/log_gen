ae29c849da87739b428f465368f3dfe817b5d1e2
wacadey <wacadey@gmail.com>
2026-08-21 15:12:52 +0900

    Feat: flink/firehose iam role  구성

--- infra/iam-flink.tf
+++ infra/iam-flink.tf
@@ -92,3 +92,56 @@ resource "aws_iam_role_policy" "firehose" {
   role   = aws_iam_role.flink.id
   policy = data.aws_iam_policy_document.flink.json
 }
+
+
+# silver layer 에서 사용하는 firehose role
+data "aws_iam_policy_document" "firehose_silver_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["firehose.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role" "firehose_silver" {
+  name = "${var.project_name}-firehose-silver-role"
+  assume_role_policy = data.aws_iam_policy_document.firehose_silver_assume.json
+}
+data "aws_iam_policy_document" "firehose_silver" {
+  # silver kinesis 읽기 권한 관련  
+  statement {
+    effect = "Allow"
+    actions = [
+      "kinesis:DescribeStream",
+      "kinesis:DescribeStreamSummary",
+      "kinesis:GetShardIterator",
+      "kinesis:GetRecords",
+      "kinesis:ListShards"
+    ]
+    resources = [
+      aws_kinesis_stream.silver.arn
+    ]
+  }
+  # s3 저장 권한 관련
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:GetBucketLocation",
+      "s3:ListBucket",
+      "s3:PutObject"
+    ]
+    resources = [
+      aws_s3_bucket.data.arn,       # 해당 버킷
+      "${aws_s3_bucket.data.arn}/*" # 해당 버킷 이하 모든 경로
+    ]
+  }
+}
+resource "aws_iam_role_policy" "firehose_silver" {
+  name   = "${var.project_name}-firehose-silver-s3-policy"
+  role   = aws_iam_role.firehose_silver.id
+  policy = data.aws_iam_policy_document.firehose_silver.json
+}
