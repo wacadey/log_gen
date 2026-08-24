f929b5f5d1328a5abb6bc7d3209abc3a39cf48e1
wacadey <wacadey@gmail.com>
2026-08-24 14:37:59 +0900

    Feat: 새로 추가될 주요 리소스명 정의

--- infra/locals.tf
+++ infra/locals.tf
@@ -30,3 +30,7 @@ locals {
 
 # [실버 + 오염데이터처리 추가] , rejected 프리픽스 추가
 # 리소스명 2개 구성
+locals {
+  rejected_kinesis_stream_name = "${var.project_name}-rejected-kinesis"
+  rejected_firehose_name       = "${var.project_name}-rejected-firehose"
+}
